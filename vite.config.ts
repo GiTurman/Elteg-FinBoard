5437735ec81ee7d2b7b868c95d30e5853ab0bd60
GiTurman <giorgi.turman@gmail.com>
2026-03-03 12:42:53 +0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,9 +10,12 @@ export default defineConfig(({ mode }) => {
         host: '0.0.0.0',
       },
       plugins: [react()],
+      define: {
+        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+      },
       resolve: {
         alias: {
-          // ეს ხაზი უზრუნველყოფს, რომ @ ყოველთვის მთავარ საქაღალდეზე მიუთითებდეს
           "@": path.resolve(__dirname, "./"),
         }
       }
